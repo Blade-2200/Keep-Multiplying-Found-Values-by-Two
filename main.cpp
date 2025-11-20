2f2dd6f12f54c119c77b30bef491edfbdc307403
Blade-2200 <daniel.lee.sedu@gmail.com>
2025-11-19 17:34:12 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        while (std::find(nums.begin(), nums.end(), original) != nums.end()) {
+            original = original * 2;
+        }   
+        return original;
+    }
+};
